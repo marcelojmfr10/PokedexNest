a112dcb90c1c55c0e37e040e729d5dbae07e71e6
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-01 21:24:03 -0600

    Paquete joi y env.template

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,22 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 import { join } from 'path';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB!),
+    MongooseModule.forRoot(process.env.MONGODB!, {
+      // dbName: 'pokemonsdb'
+    }),
 
     PokemonModule,
 
---
+++ src/config/joi.validation.ts
@@ -0,0 +1,12 @@
+
+
+import * as Joi from 'joi';
+
+
+export const JoiValidationSchema = Joi.object({
+    MONGODB: Joi.required(),
+    PORT: Joi.number().default(3000),
+    DEFAULT_LIMIT: Joi.number().default(6),
+});
+
+
